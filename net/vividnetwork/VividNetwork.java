1543acc0783871cad6275966fbd4dc48610f7f22
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-29 18:49:21 -0500

    Update VividNetwork.java

--- net/vividnetwork/VividNetwork.java
+++ net/vividnetwork/VividNetwork.java
@@ -2,12 +2,13 @@ import net.vividnetwork.Commands.HReloadCommand;
 import net.vividnetwork.Listeners.DoubleJumpListener;
 import net.vividnetwork.Listeners.DropListener;
 import net.vividnetwork.Listeners.JoinLeaveListener;
+import net.vividnetwork.Listeners.VanishTorchListener;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class VividNetwork extends JavaPlugin implements Listener{
+public class VividNetwork extends JavaPlugin{
 
 		@Override
 		public void onEnable() {
@@ -15,6 +16,7 @@ public class VividNetwork extends JavaPlugin implements Listener{
 			getServer().getPluginManager().registerEvents(new JoinLeaveListener(this), this);
 			getServer().getPluginManager().registerEvents(new DropListener(this), this);
 			getServer().getPluginManager().registerEvents(new DoubleJumpListener(this), this);
+			getServer().getPluginManager().registerEvents(new VanishTorchListener(this), this);
 			//Commands
 			getCommand("hreload").setExecutor(new HReloadCommand(this));
 			PluginDescriptionFile pdfFile = this.getDescription();
