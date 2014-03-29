794802cc1357db7e43952f86893eafee5ddf97c2
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-29 17:55:10 -0500

    Create VividNetwork.java

---
+++ net/vividnetwork/VividNetwork.java
@@ -0,0 +1,32 @@
+import net.vividnetwork.Commands.HReloadCommand;
+import net.vividnetwork.Listeners.DoubleJumpListener;
+import net.vividnetwork.Listeners.DropListener;
+import net.vividnetwork.Listeners.JoinLeaveListener;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VividNetwork extends JavaPlugin implements Listener{
+
+		@Override
+		public void onEnable() {
+			//Listeners			
+			getServer().getPluginManager().registerEvents(new JoinLeaveListener(this), this);
+			getServer().getPluginManager().registerEvents(new DropListener(this), this);
+			getServer().getPluginManager().registerEvents(new DoubleJumpListener(this), this);
+			//Commands
+			getCommand("hreload").setExecutor(new HReloadCommand(this));
+			PluginDescriptionFile pdfFile = this.getDescription();
+			System.out.println("[VividNetwork] "+pdfFile.getVersion()+" is Enabled!");
+			saveConfig();
+		}
+		
+
+		@Override
+		public void onDisable(){
+			PluginDescriptionFile pdfFile = this.getDescription();
+			System.out.println("[VividNetwork] "+pdfFile.getVersion()+" is Disabled!");
+			saveConfig();
+		}
+}
