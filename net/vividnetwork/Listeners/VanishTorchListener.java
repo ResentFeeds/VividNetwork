05a0008c5572a867f5710e5f32631eeddeb40275
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-29 23:04:03 -0500

    Update VanishTorchListener.java

--- net/vividnetwork/Listeners/VanishTorchListener.java
+++ net/vividnetwork/Listeners/VanishTorchListener.java
@@ -26,26 +26,35 @@ public class VanishTorchListener implements Listener{
 		
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event){
+    	if(!(event.getPlayer().hasPermission("vanishtorch.use")))
+    		return;
+    	if(event.getItem() == null || event.getItem().equals(Material.AIR))
+            return;
     if (event.getPlayer().hasPermission("vanishtorch.use")) {
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (event.getItem().getType() == Material.MAGMA_CREAM) {
+            if (event.getItem().getType().equals(Material.MAGMA_CREAM)){
                 Player player = event.getPlayer();
                 for (Player other : plugin.getServer().getOnlinePlayers()) {
                     player.hidePlayer(other);
                     player.playSound(player.getLocation(), Sound.ANVIL_LAND, 10, 1);
                     player.setItemInHand(new ItemStack(Material.SLIME_BALL));
-                    player.sendMessage(ChatColor.GRAY + "Players toggled: "+ChatColor.RED+"Off"+ChatColor.GRAY+"!");
+                    player.sendMessage(ChatColor.GRAY + "Players toggled: "+ChatColor.RED.toString()+ChatColor.BOLD+"Off"+ChatColor.GRAY+"!");
                     return;
                 }
                 return;
             }
-            if (event.getItem().getType() == Material.SLIME_BALL) {
+            
+            if(event.getItem() == null || event.getItem().equals(Material.AIR))
+              return;
+              if(event.getItem().getType() != Material.SLIME_BALL)
+            	  return;
+            if (event.getItem().getType().equals(Material.SLIME_BALL)){
                 Player player = event.getPlayer();
                 for (Player other : plugin.getServer().getOnlinePlayers()) {
                     player.showPlayer(other);
                     player.playSound(player.getLocation(), Sound.ANVIL_BREAK, 10, 1);
                     player.setItemInHand(new ItemStack(Material.MAGMA_CREAM));
-                    player.sendMessage(ChatColor.GRAY + "Players toggled: "+ChatColor.GREEN+"On"+ChatColor.GRAY+"!");
+                    player.sendMessage(ChatColor.GRAY + "Players toggled: "+ChatColor.GREEN.toString()+ChatColor.BOLD+"On"+ChatColor.GRAY+"!");
                     return;
                 }
                 return;
